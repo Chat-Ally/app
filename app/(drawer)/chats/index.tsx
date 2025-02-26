e676f4770c04d9d2f9d6cd59aee3d8f88d8f1c78
Daniel Escobar <daanielescobar@gmail.com>
2025-02-25 23:12:53 -0600

    get data from backend

--- app/(drawer)/chats/index.tsx
+++ app/(drawer)/chats/index.tsx
@@ -1,52 +1,69 @@
+import { useEffect, useState } from "react";
 import { View, Text, FlatList, Pressable } from "react-native";
+import { getConversations } from "~/lib/enwhats";
 
-const conversations = [
-    { id: "1", name: "Andrea", time: "Hace 1 min", number: 5 },
-    { id: "2", name: "Pepe", time: "Hace 2 min", number: 3 },
-    { id: "3", name: "Pedro", time: "Hace 10 min", number: 8 },
-];
+interface ChatNameProps {
+    text: string
+}
 
-export default function Chats() {
-    const handlePress = (item) => {
+function ChatName({ text }: ChatNameProps) {
+    return <Text className="text-black dark:text-white">{text}</Text>
+}
+
+export default async function Chats() {
+    const handlePress = (item: any) => {
         console.log("Pressed", item.name);
     };
 
+    const [chats, setChats] = useState<any>()
+
+    useEffect(() => {
+        getConversations().then((conversations: any) => {
+            setChats(conversations)
+        }).catch(error => {
+            console.error(error)
+        })
+    }, [])
+
     return (
         <View className="flex-1 p-4 bg-white dark:bg-black">
-            <FlatList
-                data={conversations}
-                keyExtractor={(item) => item.id}
-                renderItem={({ item }) => (
-                    <Pressable
-                        onPress={() => handlePress(item)}
-                        android_ripple={{ color: "#ddd" }} 
-                        style={({ pressed }) => [
-                            {
-                                backgroundColor: pressed ? "#f0f0f0" : "white", 
-                            },
-                            { 
-                                paddingVertical: 16,  // Aumentando el tamaño vertical
-                                paddingHorizontal: 12, // Añadiendo espacio horizontal
-                                borderBottomWidth: 1, 
-                                borderColor: "#e0e0e0",
-                                marginBottom: 8, // Separar entre elementos
-                            },
-                        ]}
-                    >
-                        <View style={{ flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center' }}>
-                            <View>
-                                <Text className="text-lg font-semibold dark:text-white">{item.name}</Text>
-                                <Text className="text-gray-500 dark:text-white">{item.number} mensajes</Text> {/* Mostrar el número debajo del nombre */}
-                            </View>
-                            <View style={{ alignItems: 'flex-end' }}>
-                                <Text className="text-gray-500 dark:text-white" style={{ textAlign: 'right' }}>
-                                    {item.time}
-                                </Text>
-                            </View>
-                        </View>
-                    </Pressable>
-                )}
-            />
-        </View>
+            {
+                chats ?
+                    <FlatList
+                        data={chats}
+                        keyExtractor={(item) => item.id}
+                        renderItem={({ item }) => (
+                            <Pressable
+                                onPress={() => handlePress(item)}
+                                android_ripple={{ color: "#ddd" }}
+                                style={({ pressed }) => [
+                                    {
+                                        backgroundColor: pressed ? "#f0f0f0" : "white",
+                                    },
+                                    {
+                                        paddingVertical: 16,
+                                        paddingHorizontal: 12,
+                                        borderBottomWidth: 1,
+                                        borderColor: "#e0e0e0",
+                                        marginBottom: 8,
+                                    },
+                                ]}
+                            >
+                                <View style={{ flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center' }}>
+                                    {
+                                        item.customer_name ?
+                                            <ChatName text={item.customer_name} /> :
+                                            <ChatName text="Sin nombre" />
+                                    }
+                                    <Text className="text-lg font-semibold dark:text-white">{item.customer_phone}</Text>
+                                </View>
+                            </Pressable>
+                        )}
+                    />
+                    :
+                    (<Text> Loading</Text>)
+            }
+
+        </View >
     );
 }
