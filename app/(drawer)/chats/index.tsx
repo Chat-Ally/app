cf27132347977802c16edbbe7e4fa544914c44c6
Daniel Escobar <daanielescobar@gmail.com>
2025-04-05 17:16:56 -0600

    improve styles

--- app/(drawer)/chats/index.tsx
+++ app/(drawer)/chats/index.tsx
@@ -1,10 +1,9 @@
 import { Link, useNavigation } from 'expo-router';
 import React, { useEffect, useState } from 'react';
-import { View, FlatList } from 'react-native';
+import { View, FlatList, TouchableOpacity } from 'react-native';
 import { Text } from '~/components/ui/text';
 import { supabase } from "../../../lib/supabase";
 import { Avatar, AvatarFallback } from '~/components/ui/avatar';
-import { H3 } from '~/components/ui/typography';
 
 interface Chat {
     id: string;
@@ -18,6 +17,17 @@ interface Chat {
 export default function Chats() {
     const [chats, setChats] = useState<Chat[]>([]);
     const navigation = useNavigation();
+
+
+    useEffect(() => {
+        navigation.setOptions({
+            title: "Chats",
+            headerStyle: {
+                elevation: 0
+            }
+        })
+    }, [])
+
 
     useEffect(() => {
         const fetchChats = async () => {
@@ -47,12 +57,16 @@ export default function Chats() {
     }, []);
 
     return (
-        <View style={{ flex: 1 }}>
+        <View style={{ flex: 1 }} className=''>
             <FlatList
+                style={{ flex: 1 }}
+                className='px-2'
                 data={chats}
                 keyExtractor={(item) => item.id}
                 renderItem={({ item }) => (
                     <Link
+                        style={{ flex: 1 }}
+                        className='border-b border-gray-200  mt-2 pb-2'
                         href={{
                             pathname: '/(drawer)/chats/[chatId]',
                             params: {
@@ -62,15 +76,17 @@ export default function Chats() {
                             }
                         }}
                     >
-                        <Avatar alt="Zach Nugent's Avatar" className='mr-2'>
-                            {/* <AvatarImage source={{ uri: GITHUB_AVATAR_URI }} /> */}
-                            <AvatarFallback>
-                                <Text>{(item.customer_name?.slice(0, 2) || item.phones?.number?.slice(3, 5)) || 'desconocido'}</Text>
-                            </AvatarFallback>
-                        </Avatar>
-                        <View className='ml-2'>
-                            <H3 role='heading' >{item.customer_name || item.phones.number || 'desconocido'}</H3>
-                            <Text>{new Date(item.created_at).toLocaleString()}</Text>
+                        <View style={{ flex: 1 }} className='w-full flex-row items-center '>
+                            <Avatar alt="Zach Nugent's Avatar" className='mr-2 w-16 aspect-square '>
+                                {/* <AvatarImage source={{ uri: GITHUB_AVATAR_URI }} /> */}
+                                <AvatarFallback>
+                                    <Text>{(item.customer_name?.slice(0, 2) || item.phones?.number?.slice(3, 5)) || 'desconocido'}</Text>
+                                </AvatarFallback>
+                            </Avatar>
+                            <View >
+                                <Text role='heading' className=' text-2xl' >{item.customer_name || item.phones.number || 'desconocido'}</Text>
+                                <Text className=' text-gray-500'>{new Date(item.created_at).toLocaleString()}</Text>
+                            </View>
                         </View>
                     </Link>
                 )}
